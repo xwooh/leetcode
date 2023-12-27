2022aea45d78b588384bf0a388757184df2798cf
Yuan <isyuan@yeah.net>
2023-12-27 17:29:36 +0800

    优雅的链表相交

--- go/0160.intersection-of-two-linked-lists/solution.go
+++ go/0160.intersection-of-two-linked-lists/solution.go
@@ -15,52 +15,28 @@ import (
 // @lc code=begin
 
 func getIntersectionNode(headA, headB *ListNode) (ans *ListNode) {
-	if headA == nil || headB == nil {
-		return
-	}
-
 	h1, h2 := headA, headB
 
-	for h1 != nil && h2 != nil {
-		if h1 == h2 {
-			return h1
+	// 如果有交点则在交点处退出
+	// 如果没交点，因为 A+B 长度 == B+A 长度，所以最终也会在 nil 处相遇(相等)
+	for h1 != h2 {
+		if h1 == nil {
+			// h1 走完了，就去到 headB 起始处
+			h1 = headB
+		} else {
+			h1 = h1.Next
 		}
 
-		h1 = h1.Next
-		h2 = h2.Next
-	}
-
-	if h1 == nil {
-		h1 = headB
-	}
-	if h2 == nil {
-		h2 = headA
-	}
-
-	for h1 != nil && h2 != nil {
-		if h1 == h2 {
-			return h1
+		if h2 == nil {
+			// h2 走完了，就去到 headA 起始处
+			h2 = headA
+		} else {
+			h2 = h2.Next
 		}
 
-		h1 = h1.Next
-		h2 = h2.Next
 	}
 
-	if h1 == nil {
-		h1 = headB
-	}
-	if h2 == nil {
-		h2 = headA
-	}
-
-	for h1 != nil && h2 != nil {
-		if h1 == h2 {
-			return h1
-		}
-
-		h1 = h1.Next
-		h2 = h2.Next
-	}
+	ans = h1
 
 	return
 }
